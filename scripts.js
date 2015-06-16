b3b806c9c235a7bf2ca6df40494ed8ae4dc0720c
Darwin Otoniel Mateos Sánchez <ethan06@gmail.com>
2015-06-16 11:34:47 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,3 +3,18 @@ $(document).ready(function(){
 		return reemplaza.replace('Hola', '<strong>Mi</strong>');
 	});
 });
+
+//Change background color
+function changeColor(){
+    if ( $(".slide1").css("display") == "block" ){
+        $('.imgwrap').css({'background-color':'red'});
+    }else{
+        if ( $(".slide2").css("display") == "block" ){
+            $('.imgwrap').css({'background-color':'green'});
+        }else{
+            $('.imgwrap').css({'background-color':'blue'});
+        }
+    }
+}
+//Function change background imgwrap
+setInterval(changeColor, 5000);
